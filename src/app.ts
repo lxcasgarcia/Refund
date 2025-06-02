9d290857af590991406517fd5c58e2cbb1e4c201
Lucas <lxcasgarcia09@gmail.com>
2025-06-02 16:15:18 -0300

    Tratamento das exceções

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
+import { errorHandling } from "./middlewares/error-handling";
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-})
+app.use(errorHandling);
 
 export { app }
