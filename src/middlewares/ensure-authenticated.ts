b255b18142a44f11e3bcfb482b94fda5f89c217a
Lucas <lxcasgarcia09@gmail.com>
2025-06-03 14:42:10 -0300

    ensure-authenticated

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,35 @@
+import { verify } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
+import { NextFunction, Request, Response } from "express";
+
+interface TokenPayload {
+    role: string
+    sub: string
+}
+
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    try {
+        const authHeader = request.headers.authorization
+
+        if (!authHeader) {
+            throw new AppError("Token não encontrado", 401);
+        }
+
+        const [, token] = authHeader.split(" ");
+        
+        const { role, sub: user_id } = verify(token, authConfig.jwt.secret) as TokenPayload;
+
+        request.user = {
+            id: user_id,
+            role
+        }
+
+        return next();
+
+    } catch (error) {
+        throw new AppError("Token inválido", 401);
+    }
+}
+
+export { ensureAuthenticated };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { refundsRoutes } from "./refunds-routes";
 
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+
 const routes = Router();
 
 // Rotas públicas
@@ -10,6 +12,7 @@ routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
 
 // Rotas privadas
+routes.use(ensureAuthenticated);
 routes.use("/refunds", refundsRoutes);
 
 export { routes }
