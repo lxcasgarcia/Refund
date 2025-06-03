0af9edc28cdd4dbb1ca8f0e9ea74f7ec01bdac58
Lucas <lxcasgarcia09@gmail.com>
2025-06-03 13:37:36 -0300

    Rota e Controller para criar usuário

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
+import { routes } from './routes';
 import { errorHandling } from "./middlewares/error-handling";
 
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(routes);
 app.use(errorHandling);
 
 export { app }
