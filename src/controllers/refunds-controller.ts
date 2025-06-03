7a8a17620b04ed331a2897021febfde86c4fa8a4
Lucas <lxcasgarcia09@gmail.com>
2025-06-03 15:55:51 -0300

    Criando rota de listar

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -33,6 +33,11 @@ class RefundsController {
 
         response.status(201).json(refund)
     }
+
+    async index(request: Request, response: Response) {
+
+        response.json({ message: "ok"});
+    }
 }
 
 export { RefundsController };
