92d4e158536b85b5d8787f973926751e01a6f15f
Lucas <lxcasgarcia09@gmail.com>
2025-06-04 14:52:43 -0300

    Validando arquivo

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -5,7 +5,10 @@ import crypto from "node:crypto";
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3 // 3MB
+// 1K = 1024 bytes
+// 1MB = 1024 * 1024 bytes
+const MAX_SIZE = 3
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE // 3MB
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 const MULTER = {
@@ -20,4 +23,4 @@ const MULTER = {
     })
 }
 
-export default { TMP_FOLDER, UPLOADS_FOLDER, MAX_FILE_SIZE, ACCEPTED_IMAGE_TYPES, MULTER };
+export default { TMP_FOLDER, UPLOADS_FOLDER, MAX_FILE_SIZE, ACCEPTED_IMAGE_TYPES, MULTER, MAX_SIZE };
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express';
 import { z } from 'zod';
-import { prisma } from '@/database/prisma';
-import { AppError } from '@/utils/AppError';
-import { Category } from '@prisma/client';
+import uploadConfig from '@/configs/upload';
 
 class UploadController {
     async create(request: Request, response: Response) {
-        response.json({ message: "ok"})
+        try {
+            const fileSchema = z.object({
+                filename: z.string().min(1, "Arquivo é obrigatório"),
+                mimetype: z.string().
+                    refine((type) => uploadConfig.ACCEPTED_IMAGE_TYPES.
+                        includes(type), `Formato de arquivo inválido. Formatos aceitos: ${uploadConfig.ACCEPTED_IMAGE_TYPES} `),
+                size: z.number().positive().refine((size) => size <= uploadConfig.MAX_FILE_SIZE, `Tamanho máximo do arquivo é de ${uploadConfig.MAX_SIZE}`),
+            }).passthrough()
+
+            const { file } = fileSchema.parse(request.file);
+
+            response.json({ message: "Arquivo enviado com sucesso!" });
+        } catch (error) {
+            throw error
+        }
     }
 }
 
