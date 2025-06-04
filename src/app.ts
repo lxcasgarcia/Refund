bd180abcf9928055feff89772b01b0a592be41cc
Lucas <lxcasgarcia09@gmail.com>
2025-06-04 16:24:23 -0300

    Encarremaneto do projeto

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,14 @@ import "express-async-errors"
 
 import { routes } from './routes';
 import { errorHandling } from "./middlewares/error-handling";
+import uploadConfigs from './configs/upload';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfigs.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use(errorHandling);
